422adb53c2334f8744ba34a654d6f116676abb99
Kim Andersson <Kimpabooy@live.com>
2024-09-25 09:40:31 +0200

    Made a Login method

--- Bankomaten/Program.cs
+++ Bankomaten/Program.cs
@@ -7,5 +7,61 @@
 
             Console.ReadKey();
         }
+
+        private static bool Login()
+        {
+            bool userIdOk = false;
+            bool userPasswordOk = false;
+            bool logIn = false;
+
+            Console.WriteLine("Vänligen logga in");
+            Console.WriteLine();
+            Console.Write("Användarnamn: ");
+            string userId = Console.ReadLine().ToLower(); ;
+            Console.Write("Lösenord: ");
+            string userPassword = Console.ReadLine();
+
+            string[] users = { "kim", "håkan", "alexander", "yvonne", "niklas" };
+            string[] userArrPassword = { "921027", "670111", "951227", "670719", "920304" };
+
+            foreach (string item in users)
+            {
+                if (userId == item)
+                {
+                    userIdOk = true;
+                    break;
+                }
+                else
+                {
+                    userIdOk = false;
+                }
+            }
+
+            foreach (string item in userArrPassword)
+            {
+                if (userPassword == item)
+                {
+                    userPasswordOk = true;
+                    break;
+                }
+                else
+                {
+                    userPasswordOk = false;
+                }
+            }
+
+            if (userIdOk && userPasswordOk)
+            {
+                logIn = true;
+            }
+            else
+            {
+                Console.WriteLine("inte rätt användare eller lösenord.");
+                logIn = false;
+            }
+
+            return logIn;
+        }
+
     }
 }
