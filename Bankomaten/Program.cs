ccea3710426f3328c14f6ba47628361f691a9f71
Kim Andersson <Kimpabooy@live.com>
2024-09-25 11:30:03 +0200

    Made some more changes to the Account method

--- Bankomaten/Program.cs
+++ Bankomaten/Program.cs
@@ -7,159 +7,153 @@
             bool userOk = Login();
             bool currentUser = userOk;
 
-            Console.WriteLine("Välkommen till DinBank \n");
+            if (userOk)
+            {
+                Console.WriteLine("Välkommen till DinBank \n");
+            }
+
 
             while (currentUser == true)
             {
                 int userChoice = Menu();
                 Console.Clear();
-                if (userChoice == 1)
-                {
+                Console.WriteLine("Loggar ut dig, vänta lite...");
+                Thread.Sleep(1000);
+                userOk = Login();
+                currentUser = userOk;
+            }
 
-                }
-                if (userChoice == 2)
-                {
-
-                }
-                else if (userChoice == 3)
-                {
-
-                }
-                else
-                {
-                    Console.WriteLine("Loggar ut dig, vänta lite...");
-                    Thread.Sleep(1000);
-                    currentUser = false;
-                }
-            }   
 
             Console.ReadKey();
+
         }
-
-        private static bool Login()
-        {
-            bool userIdOk = false;
-            bool userPasswordOk = false;
-            bool logIn = false;
-
-            Console.WriteLine("Vänligen logga in");
-            Console.WriteLine();
-            Console.Write("Användarnamn: ");
-            string userId = Console.ReadLine().ToLower(); ;
-            Console.Write("Lösenord: ");
-            string userPassword = Console.ReadLine();
-
-            string[] users = { "kim", "håkan", "alexander", "yvonne", "niklas" };
-            string[] userArrPassword = { "921027", "670111", "951227", "670719", "920304" };
-
-            foreach (string item in users)
+            private static bool Login()
             {
-                if (userId == item)
+                bool userIdOk = false;
+                bool userPasswordOk = false;
+                bool logIn = false;
+
+                Console.WriteLine("Vänligen logga in");
+                Console.WriteLine();
+                Console.Write("Användarnamn: ");
+                string userId = Console.ReadLine().ToLower(); ;
+                Console.Write("Lösenord: ");
+                string userPassword = Console.ReadLine();
+
+                string[] users = { "kim", "håkan", "alexander", "yvonne", "niklas" };
+                string[] userArrPassword = { "921027", "670111", "951227", "670719", "920304" };
+
+                foreach (string item in users)
                 {
-                    userIdOk = true;
-                    break;
+                    if (userId == item)
+                    {
+                        userIdOk = true;
+                        break;
+                    }
+                    else
+                    {
+                        userIdOk = false;
+                    }
+                }
+
+                foreach (string item in userArrPassword)
+                {
+                    if (userPassword == item)
+                    {
+                        userPasswordOk = true;
+                        break;
+                    }
+                    else
+                    {
+                        userPasswordOk = false;
+                    }
+                }
+
+                if (userIdOk && userPasswordOk)
+                {
+                    logIn = true;
                 }
                 else
                 {
-                    userIdOk = false;
+                    Console.WriteLine("inte rätt användare eller lösenord.");
+                    logIn = false;
                 }
+
+                return logIn;
             }
 
-            foreach (string item in userArrPassword)
+            private static int Menu()
             {
-                if (userPassword == item)
+                Console.Clear();
+                Console.WriteLine("Vänligen välj önskad metod\n");
+
+                Console.WriteLine("1. Se ditt konto(n)/saldo");  // Account method? return int?
+                Console.WriteLine("2. Överför mellan konton"); // TransferBetweenAccounts method? return int?
+                Console.WriteLine("3. Ta ut-/sätt in pengar"); // WithdrawalOrDeposit method? void?
+                Console.WriteLine("4. Logga ut");              // Logout method? void?
+
+                int menu = Convert.ToInt32(Console.ReadLine());
+
+                bool userChoice = true;
+                while (userChoice)
                 {
-                    userPasswordOk = true;
-                    break;
+                    switch (menu)
+                    {
+                        case 1:
+                            Account(menu);
+                            break;
+                        case 2:
+                            menu = 2;
+                            break;
+                        case 3:
+                            menu = 3;
+                            break;
+                        case 4:
+                            menu = 4;
+                            break;
+                        default:
+                            break;
+                    }
+                    userChoice = false;
                 }
-                else
-                {
-                    userPasswordOk = false;
-                }
+                return menu;
             }
 
-            if (userIdOk && userPasswordOk)
+            private static int Account(int userChoice)
             {
-                logIn = true;
-            }
-            else
-            {
-                Console.WriteLine("inte rätt användare eller lösenord.");
-                logIn = false;
-            }
+                int accountOne = 0;
+                int accountTwo = 0;
 
-            return logIn;
-        }
-        private static int Menu()
-        {
-            Console.Clear();
-            Console.WriteLine("Vänligen välj önskad metod\n");
-
-            Console.WriteLine("1. Se ditt konto(n)/saldo");  // Account method? return int?
-            Console.WriteLine("2. Överför mellan konton"); // TransferBetweenAccounts method? return int?
-            Console.WriteLine("3. Ta ut-/sätt in pengar"); // WithdrawalOrDeposit method? void?
-            Console.WriteLine("4. Logga ut");              // Logout method? void?
-
-            int menu = Convert.ToInt32(Console.ReadLine());
-
-            bool userChoice = true;
-            while (userChoice)
-            {
-                switch (menu)
+                switch (userChoice)
                 {
                     case 1:
-                        menu = 1;
+                        accountOne = 14000; // kanske strings
+                        accountTwo = 0;
+                        Console.WriteLine($" Here are your Accounts\n #1 balance is: {accountOne}$\n #2 balance is: {accountTwo}$");
+                        Console.ReadKey();
                         break;
                     case 2:
-                        menu = 2;
+                        accountOne = 24000;
+                        Console.WriteLine($"Your Account balance is: {accountOne} ");
                         break;
                     case 3:
-                        menu = 3;
+                        accountOne = 9500;
+                        Console.WriteLine($"Your Account balance is: {accountOne} ");
                         break;
                     case 4:
-                        menu = 4;
+                        accountOne = 20000;
+                        Console.WriteLine($"Your Account balance is: {accountOne} ");
+                        break;
+                    case 5:
+                        accountOne = 30000;
+                        Console.WriteLine($"Your Account balance is: {accountOne} ");
                         break;
                     default:
                         break;
                 }
-                userChoice = false;
+                Console.ReadKey();
+                return userChoice;
             }
-            return menu;
-        }
-        private static int Account(int userChoice)
-        {
-            int accountOne = 0;
-            int accountTwo = 0;
-            switch (userChoice)
-            {
-                case 1:
-                    accountOne = 14000; // kanske strings
-                    accountTwo = 0;
-                    Console.WriteLine($" Here are your Accounts\n #1 balance is: {accountOne}$\n" +
-                                      $"                                         {accountTwo}$");
-                    Console.ReadKey();
-                    break;
-                case 2:
-                    accountOne = 24000;
-                    Console.WriteLine($"Your Account balance is: {accountOne} ");
-                    break;
-                case 3:
-                    accountOne = 9500;
-                    Console.WriteLine($"Your Account balance is: {accountOne} ");
-                    break;
-                case 4:
-                    accountOne = 20000;
-                    Console.WriteLine($"Your Account balance is: {accountOne} ");
-                    break;
-                case 5:
-                    accountOne = 30000;
-                    Console.WriteLine($"Your Account balance is: {accountOne} ");
-                    break;
-                default:
-                    break;
-            }
-            Console.ReadKey();
-            return userChoice;
-        }
+        
     }
 }
