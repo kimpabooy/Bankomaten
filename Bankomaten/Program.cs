2bf6b6ccf89c8c691fc0f7723757b6a2189e3c96
Kim Andersson <Kimpabooy@live.com>
2024-10-07 11:33:04 +0200

    I reworked the login and Menu

--- Bankomaten/Program.cs
+++ Bankomaten/Program.cs
@@ -4,46 +4,47 @@
     {
         static void Main(string[] args)
         {
+            bool menuActive = false;
+            bool activeLogin = true;
+            string activeUser;
+
             Console.WriteLine("Välkommen till DinBank \n");
-            
-            string activeUser = Login();
-                
+
+            do
+            {
+                activeUser = Login();
+
                 if (activeUser == "")
                 {
-                Console.WriteLine("Programmet stängs nu av");
-                Thread.Sleep(1500);
+                    Console.WriteLine("Programmet stängs nu av");
+                    Thread.Sleep(1500);
                 }
+                else
+                {
+                    int menuChoice = 0;
+                    while (menuChoice != 4)
+                    {
+                        menuChoice = Menu(activeUser);
+                    }
+                    
+                    //if (menuChoice == 4)
+                    //{
+                    //    break;
+                    //}
+                    //else
+                    //{
+                    //    continue;
+                    //}
+                }
+            } while (activeLogin);
 
-            bool menuActive = true;
-            
-            while (menuActive)
-            {
-                int userChoice = Menu();
-                
-                if (userChoice == 1)
-                {
-                    Account(userChoice);
-                }
-                else if (userChoice == 2)
-                {
-
-                }
-                else if (userChoice == 4)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Loggar ut dig, vänta lite...\n");
-                    Thread.Sleep(2000);
-                    Login();
-                }
-            }
-            
         }
 
         static string Login()
         {
             int count = 0;
             string activeUser = "";
-            
+
             while (count < 3)
             {
                 Console.WriteLine("Vänligen logga in");
@@ -58,36 +59,37 @@
                                      ["3","alexander","951227"],
                                      ["4","yvonne","670719"],
                                      ["5","niklas","920304"]};
-            
+
                 for (int i = 0; i < users.Length; i++)
                 {
                     if (userId == users[i][1] && userPassword == users[i][2])
                     {
-                        activeUser = (users[i][1]);
+                        activeUser = (users[i][0]);
                         return activeUser;
                     }
 
                 }
-                       count++;
+                count++;
             }
             if (count == 3)
             {
                 Console.WriteLine("Du har överskridit dina 3 försök.");
                 Console.ReadKey();
-                
+
             }
-                return activeUser;
+            return activeUser;
 
-        }    
-        static int Menu()
-            {
-                Console.Clear();
-                Console.WriteLine("Vänligen välj önskad metod\n");
+        }
+        static int Menu(string activeUser)
+        {
 
-                Console.WriteLine("1. Se ditt konto(n)/saldo");  // Account method? return int?
-                Console.WriteLine("2. Överför mellan konton"); // TransferBetweenAccounts method? return int?
-                Console.WriteLine("3. Ta ut-/sätt in pengar"); // WithdrawalOrDeposit method? void?
-                Console.WriteLine("4. Logga ut");              // Logout method? void?
+            Console.Clear();
+            Console.WriteLine("Vänligen välj önskad metod\n");
+
+            Console.WriteLine("1. Se ditt konto(n)/saldo");  // Account method? return int?
+            Console.WriteLine("2. Överför mellan konton"); // TransferBetweenAccounts method? return int?
+            Console.WriteLine("3. Ta ut-/sätt in pengar"); // WithdrawalOrDeposit method? void?
+            Console.WriteLine("4. Logga ut");              // Logout method? void?
             int menu = 0;
             try
             {
@@ -97,65 +99,75 @@
             catch (System.FormatException)
             {
 
-                
+
             }
 
             bool userChoice = true;
-                while (userChoice)
-                {
-                    switch (menu)
-                    {
-                        case 1:
-                            menu = 1;
-                            break;
-                        case 2:
-                            menu = 2;
-                            break;
-                        case 3:
-                            menu = 3;
-                            break;
-                        case 4:
-                            menu = 4;
-                            break;
-                    }
-                    userChoice = false;
-                }
-                return menu;
-            }
-        static int Account(int userChoice)
+            while (userChoice)
             {
-                int accountOne = 0;
-                int accountTwo = 0;
-                //Login(users[0]);
-                
-                switch (userChoice)
+                switch (menu)
                 {
                     case 1:
-                        accountOne = 14000; // kanske strings
-                        accountTwo = 0;
-                        Console.WriteLine($" Here are your Accounts\n #1 balance is: {accountOne}$\n #2 balance is: {accountTwo}$");
-                        Console.ReadKey();
+                        Account(Convert.ToInt32(activeUser));
                         break;
                     case 2:
-                        accountOne = 24000;
-                        Console.WriteLine($"Your Account balance is: {accountOne} ");
                         break;
                     case 3:
-                        accountOne = 9500;
-                        Console.WriteLine($"Your Account balance is: {accountOne} ");
+                        menu = 3;
                         break;
                     case 4:
-                        accountOne = 20000;
-                        Console.WriteLine($"Your Account balance is: {accountOne} ");
-                        break;
-                    case 5:
-                        accountOne = 30000;
-                        Console.WriteLine($"Your Account balance is: {accountOne} ");
-                        break;
-                    default:
+                        menu = 4;
                         break;
                 }
-                return userChoice;
+                userChoice = false;
             }
+            return menu;
+        }
+        static int Account(int userChoice)
+        {
+            int accountOne;
+            int accountTwo;
+            int accountThree;
+            int accountFour;
+            int accountFive;
+
+            switch (userChoice)
+            {
+                case 1:
+                    accountOne = 14000;
+                    Console.WriteLine($" Here are your Accounts\n #1 balance is: {accountOne}$\n ");
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    accountOne = 24000;
+                    accountTwo = 580;
+                    Console.WriteLine($" Here are your Accounts\n #1 balance is: {accountOne}$\n #2 balance is: {accountTwo}$");
+                    break;
+                case 3:
+                    accountOne = 9500;
+                    accountTwo = 580;
+                    accountThree = 580;
+                    Console.WriteLine($"Your Account balance is: #1 balance is: {accountOne}$ #2 balance is: {accountTwo}$ #3 balance is: {accountThree}$ ");
+                    break;
+                case 4:
+                    accountOne = 20000;
+                    accountTwo = 580;
+                    accountThree = 580;
+                    accountFour = 580;
+                    Console.WriteLine($"Your Account balance is: #1 balance is: {accountOne}$ #2 balance is: {accountTwo}$ #3 balance is: {accountThree}$ #4 balance is: {accountFour}$");
+                    break;
+                case 5:
+                    accountOne = 30000;
+                    accountTwo = 30000;
+                    accountThree = 30000;
+                    accountFour = 30000;
+                    accountFive = 30000;
+                    Console.WriteLine($"Your Account balance is: #1 balance is: {accountOne}$ #2 balance is: {accountTwo}$ #3 balance is: {accountThree}$ #4 balance is: {accountFour}$ #5 balance is: {accountFive}$");
+                    break;
+                default:
+                    break;
+            }
+            return userChoice;
+        }
     }
 }
